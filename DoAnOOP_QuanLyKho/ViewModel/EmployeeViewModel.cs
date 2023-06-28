3e3c8c6c6f84c7caff1c60c078b5f8c428609cc3
Mng721 <88709577+Mng721@users.noreply.github.com>
2023-06-28 20:58:01 +0700

    Update 28/06/2023

---
+++ DoAnOOP_QuanLyKho/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,189 @@
+﻿using DoAnOOP_QuanLyKho.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DoAnOOP_QuanLyKho.ViewModel
+{
+    public class EmployeeViewModel : BaseViewModel
+    {
+        private ObservableCollection<GioiTinh> _GendersList;
+        public ObservableCollection<GioiTinh> GendersList { get => _GendersList; set { _GendersList = value; OnPropertyChanged(); } }
+
+        private ObservableCollection<NhanVien> _List;
+        public ObservableCollection<NhanVien> List { get => _List; set { _List = value; OnPropertyChanged(); } }
+
+        private ObservableCollection<ChucVu> _PositionsList;
+        public ObservableCollection<ChucVu> PositionsList { get => _PositionsList; set { _PositionsList = value; OnPropertyChanged(); } }
+
+        private NhanVien _SelectedItem;
+        public NhanVien SelectedItem
+        {
+            get => _SelectedItem;
+            set
+            {
+                _SelectedItem = value;
+                OnPropertyChanged();
+                if (SelectedItem != null)
+                {
+                    DisplayName = SelectedItem.TenNV;
+                    SDT = SelectedItem.SDT;
+                    SelectedGender = SelectedItem.GioiTinh1;
+                    CMND = SelectedItem.CMND;
+                    NgaySinh = SelectedItem.NgaySinh;
+                    SelectedPosition = SelectedItem.ChucVu;
+                }
+            }
+        }
+
+        private GioiTinh _SelectedGender;
+        public GioiTinh SelectedGender
+        {
+            get => _SelectedGender;
+            set
+            {
+                _SelectedGender = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ChucVu _SelectedPosition;
+        public ChucVu SelectedPosition
+        {
+            get => _SelectedPosition;
+            set
+            {
+                _SelectedPosition = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+
+        private string _DisplayName;
+        public string DisplayName { get => _DisplayName; set { _DisplayName = value; OnPropertyChanged(); } }
+
+        private string _SDT;
+        public string SDT { get => _SDT; set { _SDT = value.Trim() ; OnPropertyChanged(); } }
+
+        private string _CMND;
+        public string CMND { get => _CMND; set { _CMND = value.Trim(); OnPropertyChanged(); } }
+
+        private DateTime _NgaySinh;
+        public DateTime NgaySinh { get => _NgaySinh; set { _NgaySinh = value; OnPropertyChanged(); } }
+        public EmployeeViewModel()
+        {
+            List = new ObservableCollection<NhanVien>(DataProvider.Ins.DB.NhanViens);
+            GendersList = new ObservableCollection<GioiTinh>(DataProvider.Ins.DB.GioiTinhs);
+            PositionsList = new ObservableCollection<ChucVu>(DataProvider.Ins.DB.ChucVus);
+
+            AddCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(DisplayName) || SelectedPosition == null)
+                    return false;
+                return true;
+
+            }, (p) => {
+                var Employee = new NhanVien() { TenNV = DisplayName, SDT = SDT, GioiTinh = SelectedGender.MaGT, CMND = CMND, NgaySinh = NgaySinh, MaCV = SelectedPosition.MaCV, };
+                DataProvider.Ins.DB.NhanViens.Add(Employee);
+                try
+                {
+                    DataProvider.Ins.DB.SaveChanges();
+                    List.Add(Employee);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            });
+
+            EditCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(DisplayName) || SelectedItem == null || SelectedGender == null || SelectedPosition == null)
+                {
+                    return false; 
+                }
+                var displayList = DataProvider.Ins.DB.NhanViens.Where(x => x.MaNV == SelectedItem.MaNV);
+                if (displayList != null && displayList.Count() != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }, (p) => {
+                var Employee = DataProvider.Ins.DB.NhanViens.Where(x => x.MaNV == SelectedItem.MaNV).SingleOrDefault();
+                Employee.TenNV = DisplayName;
+                if (SDT.Length == 10)
+                {
+                    Employee.SDT = SDT;
+                }
+                else
+                {
+                    MessageBox.Show("Số điện thoại không hợp lệ");
+                }
+                if (CMND.Length == 12 || CMND.Length == 9)
+                {
+                    Employee.CMND = CMND;
+                }
+                else
+                {
+                    MessageBox.Show("Chứng minh nhân dân không hợp lệ");
+                }
+                Employee.GioiTinh = SelectedGender.MaGT;
+                Employee.NgaySinh = NgaySinh;
+                Employee.MaCV = SelectedPosition.MaCV;
+                try
+                {
+                    DataProvider.Ins.DB.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+                SelectedItem.TenNV = DisplayName;
+            });
+
+            DeleteCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(DisplayName))
+                {
+                    return false;
+                }
+                var inputCustomerList = DataProvider.Ins.DB.PhieuNhaps.Where(x => x.MaNV == SelectedItem.MaNV);
+                var outputCustomerList = DataProvider.Ins.DB.PhieuNhaps.Where(x => x.MaNV == SelectedItem.MaNV);
+                var accountCustomerList = DataProvider.Ins.DB.PhieuNhaps.Where(x => x.MaNV == SelectedItem.MaNV);
+                if (inputCustomerList != null && inputCustomerList.Count() != 0)
+                    return false;
+                if (outputCustomerList != null && outputCustomerList.Count() != 0)
+                    return false;
+                if (accountCustomerList != null && accountCustomerList.Count() != 0)
+                    return false;
+                return true;
+
+            }, (p) => {
+                try
+                {
+                    DataProvider.Ins.DB.NhanViens.Attach(SelectedItem);
+                    DataProvider.Ins.DB.NhanViens.Remove(SelectedItem);
+                    DataProvider.Ins.DB.SaveChanges();
+                    List.Remove(SelectedItem);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            });
+        }
+    }
+}
---
+++ DoAnOOP_QuanLyKho/ViewModel/SupplyViewModel.cs
@@ -0,0 +1,156 @@
+﻿using DoAnOOP_QuanLyKho.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DoAnOOP_QuanLyKho.ViewModel
+{
+    public class SupplyViewModel : BaseViewModel
+    {
+
+        private ObservableCollection<SanPham> _List;
+        public ObservableCollection<SanPham> List { get => _List; set { _List = value; OnPropertyChanged(); } }
+
+        private ObservableCollection<DonViTinh> _UnitList;
+        public ObservableCollection<DonViTinh> UnitList { get => _UnitList; set { _UnitList = value; OnPropertyChanged(); } }
+
+        private ObservableCollection<LoaiSanPham> _TypeList;
+        public ObservableCollection<LoaiSanPham> TypeList { get => _TypeList; set { _TypeList = value; OnPropertyChanged(); } }
+
+        private SanPham _SelectedItem;
+        public SanPham SelectedItem
+        {
+            get => _SelectedItem;
+            set
+            {
+                _SelectedItem = value;
+                OnPropertyChanged();
+                if (SelectedItem != null)
+                {
+                    DisplayName = SelectedItem.TenSP;
+                    SelectedType = SelectedItem.LoaiSanPham;
+                    SelectedUnit = SelectedItem.DonViTinh;
+                }
+            }
+        }
+
+        private DonViTinh _SelectedUnit;
+        public DonViTinh SelectedUnit
+        {
+            get => _SelectedUnit;
+            set
+            {
+                _SelectedUnit = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private LoaiSanPham _SelectedType;
+        public LoaiSanPham SelectedType
+        {
+            get => _SelectedType;
+            set
+            {
+                _SelectedType = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+
+        private string _DisplayName;
+        public string DisplayName { get => _DisplayName; set { _DisplayName = value; OnPropertyChanged(); } }
+
+        private string _MatKhau;
+        public string MatKhau { get => _MatKhau; set { _MatKhau = value; OnPropertyChanged(); } }
+
+        public SupplyViewModel()
+        {
+
+            List = new ObservableCollection<SanPham>(DataProvider.Ins.DB.SanPhams);
+            UnitList = new ObservableCollection<DonViTinh>(DataProvider.Ins.DB.DonViTinhs);
+            TypeList = new ObservableCollection<LoaiSanPham>(DataProvider.Ins.DB.LoaiSanPhams);
+
+            AddCommand = new RelayCommand<object>((p) =>
+            {
+                if (SelectedUnit == null || SelectedType == null)
+                    return false;
+                return true;
+
+            }, (p) => {
+                var supply = new SanPham() { TenSP = DisplayName, MaDV = SelectedUnit.MaDV, MaLoai = SelectedType.MaLoai };
+                DataProvider.Ins.DB.SanPhams.Add(supply);
+                DataProvider.Ins.DB.SaveChanges();
+
+                List.Add(supply);
+            });
+
+            EditCommand = new RelayCommand<object>((p) =>
+            {
+                if (SelectedUnit == null || SelectedType == null || SelectedItem == null)
+                {
+                    return false;
+                }
+                var displayList = DataProvider.Ins.DB.SanPhams.Where(x => x.MaSP == SelectedItem.MaSP);
+                if (displayList != null && displayList.Count() != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }, (p) => {
+                var supply = DataProvider.Ins.DB.SanPhams.Where(x => x.MaSP == SelectedItem.MaSP).SingleOrDefault();
+                supply.TenSP = DisplayName;
+                supply.MaLoai = SelectedType.MaLoai;
+                supply.MaDV = SelectedUnit.MaDV;
+                try
+                {
+                    DataProvider.Ins.DB.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                SelectedItem.TenSP = DisplayName;
+            });
+
+            DeleteCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(DisplayName))
+                {
+                    return false;
+                }
+                var displayList = DataProvider.Ins.DB.SanPhams.Where(x => x.TenSP == DisplayName);
+                if (displayList != null && displayList.Count() != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }, (p) => {
+                try
+                {
+                    DataProvider.Ins.DB.SanPhams.Attach(SelectedItem);
+                    DataProvider.Ins.DB.SanPhams.Remove(SelectedItem);
+                    DataProvider.Ins.DB.SaveChanges();
+                    List.Remove(SelectedItem);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            });
+        }
+    }
+}
